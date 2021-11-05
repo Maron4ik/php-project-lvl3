59ab4a9ff53bacf22c86635b0111dad1f77273bb
alexeymaron <maron.alexey@gmail.com>
2021-11-05 13:40:34 +0200

    last chance 3

--- database/migrations/2021_11_02_113558_create_urls.php
+++ database/migrations/2021_11_02_113558_create_urls.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Urls extends Migration
+class CreateUrls extends Migration
 {
     /**
      * Run the migrations.
@@ -27,6 +27,6 @@ class Urls extends Migration
      */
     public function down()
     {
-        Schema::drop('urls');
+        Schema::dropIfExists('urls');
     }
 }
