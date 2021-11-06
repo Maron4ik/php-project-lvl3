6b0bebb9ba9aa19d258da790ee1d06d5d325e876
alexeymaron <maron.alexey@gmail.com>
2021-11-06 22:24:50 +0200

    18-41 fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,7 +48,7 @@
                     <h1 class="display-3">Анализатор страниц</h1>
                     <p class="lead">Бесплатно проверяйте сайты на SEO пригодность</p>
                     <form action="{{ route('urls.store') }}" method="post" class="d-flex justify-content-center">
-{{--                        @csrf--}}
+                        @csrf
                         <input type="text" name="url[name]" value="" class="form-control form-control-lg"
                                placeholder="https://www.example.com">
                         <button type="submit" class="btn btn-lg btn-primary ml-3 px-5 text-uppercase">Проверить</button>
