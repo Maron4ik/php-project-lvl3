48494c3a80661fa9b9d497c10a0f403b6d14dac8
alexeymaron <maron.alexey@gmail.com>
2021-11-09 08:03:20 +0200

    dander message fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,37 +1,33 @@
 @extends('layouts.base')
 
 @section('content')
-    <div>
+
         @if($errors->any())
-            <div class="alert alert-danger fs-2">
-                <ul>
-                    @foreach($errors->all() as $message)
-                        <li>
-                            {{ $message }}
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
+            <ul class="list-group">
+                @foreach($errors->all() as $message)
+                    <li class="list-group-item list-group-item-danger">
+                        {{ $message }}
+                    </li>
+                @endforeach
+            </ul>
         @endif
-        @include('flash::message')
-    </div>
 
-
-<div class="jumbotron jumbotron-fluid bg-dark">
-     <div class="container-lg">
-        <div class="row">
-            <div class="col-12 col-md-10 col-lg-8 mx-auto text-white">
-                <h1 class="display-3">Анализатор страниц</h1>
-                <p class="lead">Бесплатно проверяйте сайты на SEO пригодность</p>
-                <form action="{{ route('urls.store') }}" method="post" class="d-flex justify-content-center">
-                    @csrf
-                    <input type="text" name="url[name]" value="" class="form-control form-control-lg"
-                           placeholder="https://www.example.com">
-                    <button type="submit" class="btn btn-lg btn-primary ml-3 px-5 text-uppercase">Проверить</button>
-                </form>
+    <div class="jumbotron jumbotron-fluid bg-dark">
+        <div class="container-lg">
+            <div class="row">
+                <div class="col-12 col-md-10 col-lg-8 mx-auto text-white">
+                    <h1 class="display-3">Анализатор страниц</h1>
+                    <p class="lead">Бесплатно проверяйте сайты на SEO пригодность</p>
+                    <form action="{{ route('urls.store') }}" method="post" class="d-flex justify-content-center">
+                        @csrf
+                        <input type="text" name="url[name]" value="" class="form-control form-control-lg" id="hostname"
+                               placeholder="https://www.example.com">
+                        <label for="hostname"></label>
+                        <button type="submit" class="btn btn-lg btn-primary ml-3 px-5 text-uppercase">Проверить</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 @endsection
